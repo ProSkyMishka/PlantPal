0617f243014dde404b64eb14674fd21d8236f3c9
Lyudmila Rezunik <lrezunik@edu.hse.ru>
2024-07-10 09:45:27 +0300

    add boilerplate camera view

--- PlantApp/PlantApp/AddPlant/AddPlantView.swift
+++ PlantApp/PlantApp/AddPlant/AddPlantView.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct AddPlantView: View {
+    
+    @State private var image: UIImage?
+    @State private var recognizedPlant: PlantBaseModel?
+    @State private var isInfoLoading = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if (image == nil) {
+                CameraView(image: $image)
+            } else {
+                VStack {
+                    // View of taken picture
+                    // Image
+                    // Button to retake the image
+                    
+                    if (isInfoLoading) {
+                        ProgressView()
+                    } else {
+                        // Show plant info
+                    }
+                }
+                .onAppear {
+                    recognizePlant()
+                }
+            }
+        }
     }
-}
-
-struct AddPlantView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddPlantView()
+    
+    
+    
+    func recognizePlant() {
+        
+        // Send image data to ML model
+        
+        // get MLID from the ML model and get the plant by MLID from server
+        
+        // Change isInfoLoading to false
     }
+    
 }
---
+++ PlantApp/PlantApp/AddPlant/CameraView.swift
@@ -0,0 +1,58 @@
+//
+//  CameraView.swift
+//  PlantApp
+//
+//  Created by Lucy Rez on 10.07.2024.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct CameraView: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    
+    typealias UIViewControllerType = UIImagePickerController
+    
+    func makeUIViewController(context: Context) ->  UIViewControllerType {
+        let viewController = UIViewControllerType()
+        viewController.delegate = context.coordinator
+        viewController.sourceType = .camera
+        viewController.cameraDevice = .rear
+        viewController.cameraOverlayView = .none
+        
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> CameraView.Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+}
+
+extension CameraView {
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: CameraView
+        
+        init(parent: CameraView) {
+            self.parent = parent
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.parent.image = nil
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                
+                // Here we can resize the image if we want (or change the image ratio)
+                self.parent.image = image
+            }
+        }
+    }
+}

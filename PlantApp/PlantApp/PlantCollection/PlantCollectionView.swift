36ff663ff4791bb6a2ebdd0621bb883dfe28ebf1
Lyudmila Rezunik <lrezunik@edu.hse.ru>
2024-07-11 06:50:59 +0300

    Add delete animation to collection view (by Roman)

--- PlantApp/PlantApp/PlantCollection/PlantCollectionView.swift
+++ PlantApp/PlantApp/PlantCollection/PlantCollectionView.swift
@@ -11,6 +11,8 @@ struct PlantCollectionView: View {
     @StateObject var collectionViewModel = PlantCollectionViewModel()
     @State var flag = false
     @State var sorted_enabled: [PlantBaseModel] = []
+    @State private var offsets: [UUID: CGSize] = [:]
+    @State private var showDeleteIcons: [UUID: Bool] = [:]
     @Binding var barHidden: Bool
     
     var body: some View {
@@ -76,6 +78,33 @@ struct PlantCollectionView: View {
                         .tint(.black)
                         .padding(.vertical, 17)
                     }
+                    .offset(x: offsets[flower.id]?.width ?? 0)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                if gesture.translation.width < 0 {
+                                    offsets[flower.id] = gesture.translation
+                                    showDeleteIcons[flower.id] = true
+                                }
+                            }
+                            .onEnded { _ in
+                                if offsets[flower.id]?.width ?? 0 < -100 {
+                                    withAnimation(.easeInOut) {
+                                        if let index = collectionViewModel.plants.firstIndex(where: { $0.id == flower.id }) {
+                                            offsets[flower.id] = .zero
+                                            showDeleteIcons[flower.id] = false
+                                            collectionViewModel.plants.remove(at: index)
+                                        }
+                                    }
+                                } else {
+                                    withAnimation {
+                                        offsets[flower.id] = .zero
+                                        showDeleteIcons[flower.id] = false
+                                    }
+                                }
+                            }
+                    )
+                    .animation(.easeInOut, value: offsets[flower.id])
                 }
                 
             }
